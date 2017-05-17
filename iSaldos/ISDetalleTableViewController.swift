709f8e660a1707aa87e4c01a94b30fddb3e3e2ec
formador <formador@aula8PovMacNfor.local>
2017-05-17 21:53:28 +0200

    +[iOS] - mejoras en Vista Detalle de Ofertas -

--- iSaldos/ISDetalleTableViewController.swift
+++ iSaldos/ISDetalleTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class ISDetalleTableViewController: UITableViewController {
     
@@ -14,88 +15,80 @@ class ISDetalleTableViewController: UITableViewController {
     var oferta : ISPromocionesModel?
     var detalleImagenData : UIImage?
     
+    //MARK: - IBOutlets
     
-
+    @IBOutlet weak var myImagenOferta: UIImageView!
+    @IBOutlet weak var myNombreOferta: UILabel!
+    @IBOutlet weak var myFechaOferta: UILabel!
+    @IBOutlet weak var myInformacionOferta: UILabel!
+    @IBOutlet weak var myNombreAsociado: UILabel!
+    @IBOutlet weak var myDescripcionAsociado: UILabel!
+    @IBOutlet weak var myDireccionAsociado: UILabel!
+    @IBOutlet weak var myTelefonoFijoAsociado: UILabel!
+    @IBOutlet weak var myMovilAsociado: UILabel!
+    @IBOutlet weak var myWebAsociado: UILabel!
+    @IBOutlet weak var myEmailAsociado: UILabel!
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        //TODO: - Altura general de las celdas
+        tableView.estimatedRowHeight = 60
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        myImagenOferta.image = detalleImagenData
+        myNombreOferta.text = oferta?.nombre
+        myFechaOferta.text = oferta?.fechaFin
+        myInformacionOferta.text = oferta?.masInformacion
+        
+        myNombreAsociado.text = oferta?.asociado?.nombre
+        myDescripcionAsociado.text = oferta?.asociado?.descripcion
+        myDireccionAsociado.text = oferta?.asociado?.direccion
+        myTelefonoFijoAsociado.text = oferta?.asociado?.telefonoFijo
+        myMovilAsociado.text = oferta?.asociado?.telefonoMovil
+        myWebAsociado.text = oferta?.asociado?.web
+        myEmailAsociado.text = oferta?.asociado?.mail
+        
+        //Mapa
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.352494, longitude: -3.809620),
+                                        span: MKCoordinateSpan(latitudeDelta: 0.001,
+                                                               longitudeDelta: 0.001))
+        myMapView.setRegion(region, animated: true)
+        
+        let customAnnot = MKPointAnnotation()
+        customAnnot.coordinate = CLLocationCoordinate2D(latitude: 40.352494, longitude: -3.809620)
+        customAnnot.title = oferta?.nombre
+        customAnnot.subtitle = oferta?.asociado?.direccion
+        myMapView.addAnnotation(customAnnot)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 1 && indexPath.row == 1{
+            return UITableViewAutomaticDimension
+        }
+        return super.tableView(tableView, heightForRowAt: indexPath)
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
 
 }
